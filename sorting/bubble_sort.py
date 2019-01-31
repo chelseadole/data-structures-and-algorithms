f95c9542909d774038a4be99e8fbd41311940af6
Chelsea Dole <chelsea.dole@zonarsystems.com>
2019-01-31 14:23:39 -0800

    adding formatting for if name is main

--- sorting/bubble_sort.py
+++ sorting/bubble_sort.py
@@ -9,6 +9,8 @@ NOTES:
 
 """
 
+import sys, ast
+
 def bubble_sort(lst):
     """Sort list by using BubbleSort algorithm."""
     for i in range(len(lst)):
@@ -20,32 +22,27 @@ def bubble_sort(lst):
 
 if __name__ == '__main__':
     import timeit as ti
-    import random
-    best_case = list(range(8))
-    worst_case = list(range(8))[::-1]
-    random_case = [random.randint(0, 8) for i in range(8)]
 
-    print('Running best case...')
-    best_time = ti.timeit("bubble_sort(best_case)",
-                       setup="from __main__ import best_case, bubble_sort")
+    try:
+        if len(sys.argv) > 1:
+            input_list = sys.argv[1].split()
+            input_list = [int(i) for i in input_list]
+        else:
+            input_list = [3, 5, 2, 10, 22, 4, 23]
 
-    print('Running worst case...')
-    worst_time = ti.timeit("bubble_sort(worst_case)",
-                       setup="from __main__ import worst_case, bubble_sort")
+        original_list = input_list.copy()
 
-    print('Running random case...')
-    random_time = ti.timeit("bubble_sort(random_case)",
-                       setup="from __main__ import random_case, bubble_sort")
+        print('Sorting array...')
+        sort_time = ti.timeit("bubble_sort(input_list)",
+                              setup="from __main__ import input_list, bubble_sort")
 
-    print(f"""
-        BubbleSort! 
-        
-        Input: [0, 1, 2, 3, 4, 5, 6, 7, 8]
-        Sort time: {best_time} seconds
-        
-        Input: [8, 7, 6, 5, 4, 3, 2, 1, 0]
-        Sort time: {worst_time} seconds
-        
-        Input: List of 8 randomized integers between 0 and 8
-        Sort time: {random_time} seconds
-    """)
+        print(f"""
+            BubbleSort! 
+            
+            Input: {original_list}
+            Sort time: {sort_time} secs
+            Output: {input_list}
+            
+        """)
+    except:
+        print("""Input numbers to sort must be formatted as a string of separated numbers. \nExample: \n$ python bubble_sort.py '1 2 3 4 5'""")
