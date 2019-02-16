7763780132d0dbac517638d6a7a127985c6c5890
CHELSEA DOLE <chelseadole@gmail.com>
2019-02-15 21:48:35 -0800

    moar linked list

--- data-structures/singly_linked_list.py
+++ data-structures/singly_linked_list.py
@@ -13,15 +13,51 @@ class Node:
 class LinkedList:
     """Singly linked list."""
 
-    def __init__(self, head=None, tail=None):
+    def __init__(self, head=None):
         """Initialization of LinkedList class."""
-        self.head = head
-        self.tail = tail
+        if head:
+            self.head = Node(head)
+        else:
+            self.head = None
+        self.tail = None
+        self.length = 0
 
     def insert(self, data):
         """Insert a node into a linked list."""
-        new_node = Node(data)
-        new_node.next = self.head
-        self.head = new_node
+        new_node = Node(data, self.head, None)
         if not self.head:
             self.tail = new_node
+        self.head = new_node
+        self.length += 1
+
+    def size(self):
+        """Return length of List object."""
+        return self.length
+
+    def search(self, val):
+        """Check to see if a Node with data "val" is in List."""
+        curr = self.head
+        iters = 1
+        while curr:
+            iters += 1
+            if curr.value == val:
+                return curr, iters
+            curr = curr.next
+        return curr
+
+    def delete(self, val):
+        """Check to see if Node is in List. If it is, delete it."""
+        prev = None
+        curr = self.head
+        while curr:
+            if curr.value == val:
+                if prev:
+                    prev.next = curr.next
+                    return 'Deleted {}'.format(val)
+            prev = curr
+            curr = curr.next
+        return '{} not found in LinkedList'.format(val)
+
+
+
+
