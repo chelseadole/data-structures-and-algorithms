f693993c133a4131413982f71c798741c61b8c2a
Chelsea Dole <chelsea.dole@zonarsystems.com>
2019-02-22 16:14:50 -0800

    added hash table, and supporting methods to singly linked list.

---
+++ data_structures/hash_table.py
@@ -0,0 +1,45 @@
+"""Implementation of a hash table structure in Python.
+
+Basically a Python dictionary. Data structures comprised of "keys" which point at
+"values". When key/value pair is inserted into the hash table, the key is "hashed"
+(encoded) into a hash value. This hash value is used as an index for an array. 
+
+On "collisions" (where a hashed key is inserted into the table which already exists),
+the structure appends both the unhashed key and value into an array at that hash index. 
+Or it inserts it into a LinkedList there."""
+from data_structures.singly_linked_list import LinkedList
+
+
+class HashTable:
+
+    def __init__(self, size):
+        """Initialization of hash table of declared size."""
+        if size and isinstance(size, int):
+            self.size = size
+            self.hash_table = [LinkedList()] * self.size
+        else:
+            raise ValueError("HashTable requires paramater \"size\" of type \"int\"")
+
+    def _compress(self, key):
+        """Compress a key into a compressed hashed index value between 0 and self.size,
+        return compressed and hashed key value."""
+        return hash(key) % self.size
+
+    def insert(self, key, val):
+        """Insert a value into Hash Table."""
+        idx = self._compress(key)
+        self.hash_table[idx].insert([key, val])
+
+    def get(self, key):
+        """Retrieve the value at <KEY> of HashTable."""
+        idx = self._compress(key)
+        val = self.hash_table[idx].hash_search(key)
+        return val or "KEY: \"{}\" not in HashTable".format(key)
+
+    def delete(self, key):
+        """Delete the value at <KEY> from HashTable."""
+        idx = self._compress(key)
+        if self.hash_table[idx].hash_delete(key):
+            return "Deleted KEY: \"{}\"".format(key)
+        return "KEY: \"{}\" not in HashTable".format(key)
+
---
+++ data_structures/singly_linked_list.py
@@ -0,0 +1,82 @@
+"""Implementation of a singly linked list."""
+
+
+class Node:
+    """A single node of a linked list."""
+
+    def __init__(self, value, next_val=None):
+        """Initialization of Node class."""
+        self.value = value
+        self.next = next_val
+
+
+class LinkedList:
+    """Singly linked list."""
+
+    def __init__(self, head=None):
+        """Initialization of LinkedList class."""
+        if head:
+            self.head = Node(head)
+        else:
+            self.head = None
+        self.tail = None
+        self.length = 0
+
+    def insert(self, data):
+        """Insert a node into a linked list."""
+        new_node = Node(data, self.head)
+        if not self.head:
+            self.tail = new_node
+        self.head = new_node
+        self.length += 1
+
+    def size(self):
+        """Return length of List object."""
+        return self.length
+
+    def search(self, val):
+        """Check to see if a Node with data "val" is in List."""
+        curr = self.head
+        while curr:
+            if curr.value == val:
+                return curr
+            curr = curr.next
+        return curr
+
+    def delete(self, val):
+        """Delete a value from list."""
+        curr = self.head
+        prev = None
+        while curr:
+            if curr.value == val:
+                if prev:
+                    prev.next = curr.next
+                else:
+                    self.head = None
+                return "Deleted {}".format(val)
+            curr, prev = curr.next, curr
+        return "{} not in LinkedList".format(val)
+
+    def hash_delete(self, key):
+        """Delete specifically implemented for HashTable. Deletes with assumption that val is a list.
+        If val in list, delete and return val. If not in list, return None."""
+        curr = self.head
+        prev = None
+        while curr:
+            if curr.value[0] == key:
+                if prev:
+                    prev.next = curr.next
+                else:
+                    self.head = None
+                return curr
+            curr, prev = curr.next, curr
+
+    def hash_search(self, val):
+        """"Search specially implemented for HashTable. Searches with assumption that val is a list."""
+        curr = self.head
+        while curr:
+            if curr.value[0] == val:
+                return curr.value[1]
+            curr = curr.next
+        return curr
+
