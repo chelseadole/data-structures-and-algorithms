605017ddfd65dd6b4ad0792f611a5bec9653126c
Chelsea Dole <chelsea.dole@zonarsystems.com>
2019-03-30 21:50:33 -0700

    added stack, and added a queue from two stacks.

---
+++ practice_problems/queue_from_two_stacks.py
@@ -0,0 +1,21 @@
+"""
+Create a Queue from two stacks. First in, first out.
+"""
+
+from data_structures.stack import Stack
+
+class Queue:
+
+    def __init__(self):
+        self.s1 = Stack()
+        self.s2 = Stack()
+
+    def push(self, val):
+        while self.s1.size() > 0:
+            self.s2.push(self.s1.pop())
+        self.s2.push(val)
+        while self.s2.size() > 0:
+            self.s1.push(self.s2.pop())
+
+    def pop(self):
+        return self.s1.pop()
